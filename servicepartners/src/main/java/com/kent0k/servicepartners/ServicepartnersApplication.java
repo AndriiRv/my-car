a5c8417191405bbdc4335522cb244921eccb2e8b
Andrii Reva <rewa.andrei2010@gmail.com>
2024-02-12 19:35:06 +0200

    my-car: init commit

---
+++ servicepartners/src/main/java/com/kent0k/servicepartners/ServicepartnersApplication.java
@@ -0,0 +1,13 @@
+package com.kent0k.servicepartners;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicepartnersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServicepartnersApplication.class, args);
+    }
+
+}
---
+++ servicepartners/src/test/java/com/kent0k/servicepartners/ServicepartnersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kent0k.servicepartners;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicepartnersApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
