e59e66d8b1f7a8db94b43b6fdb068fde6248f3ab
Andrii Reva <rewa.andrei2010@gmail.com>
2024-03-05 14:31:50 +0200

    my-car: enable and setup of 'eurekaserver' microservice
    - Enabled 'eurekaserver' microservice which as Client side Discovery and Registration/service-registry.

--- eurekaserver/src/test/java/com/kent0k/eurekaserver/EurekaserverApplicationTests.java
+++ eurekaserver/src/test/java/com/kent0k/eurekaserver/EurekaserverApplicationTests.java
@@ -3,10 +3,10 @@ package com.kent0k.eurekaserver;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class EurekaserverApplicationTests {
 
-    @Test
+//    @Test
     void contextLoads() {
     }
 
